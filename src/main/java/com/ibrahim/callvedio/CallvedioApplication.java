8233ef7a8c4be5191fb10abbfe6e2e48be9d621f
ibrahim <ibrahimwinns@gmail.com>
2024-04-29 17:36:09 +0100

    api done

--- src/main/java/com/ibrahim/callvedio/CallvedioApplication.java
+++ src/main/java/com/ibrahim/callvedio/CallvedioApplication.java
@@ -7,7 +7,32 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CallvedioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CallvedioApplication.class, args);
+		SpringApplication.run(VideocallApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			UserService service
+	) {
+		return args -> {
+			service.register(User.builder()
+							.username("Ali")
+							.email("ali@mail.com")
+							.password("aaa")
+					.build());
+
+			service.register(User.builder()
+					.username("John")
+					.email("john@mail.com")
+					.password("aaa")
+					.build());
+
+			service.register(User.builder()
+					.username("Anny")
+					.email("anna@mail.com")
+					.password("aaa")
+					.build());
+		};
 	}
 
 }
