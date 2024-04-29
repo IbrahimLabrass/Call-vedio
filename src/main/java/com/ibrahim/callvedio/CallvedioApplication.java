ab3a56caa78f7952abfb8482104d5d879f147333
ibrahim <ibrahimwinns@gmail.com>
2024-04-29 16:47:56 +0100

    initial commit

---
+++ src/main/java/com/ibrahim/callvedio/CallvedioApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahim.callvedio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallvedioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallvedioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibrahim/callvedio/CallvedioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahim.callvedio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallvedioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
